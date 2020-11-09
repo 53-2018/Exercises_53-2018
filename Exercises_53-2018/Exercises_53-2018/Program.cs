2a809d6b1755082a3138efc1c679dcd82af03e1f
53-2018 <markomarkovic7788@gmail.com>
2020-11-09 12:29:43 +0100

    dodata funkcionalnost Dodaj u bazu

--- Exercises_53-2018/Exercises_53-2018/Program.cs
+++ Exercises_53-2018/Exercises_53-2018/Program.cs
@@ -17,7 +17,7 @@ namespace Exercises_53_2018
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form2());
         }
     }
 }
